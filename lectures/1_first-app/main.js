2be87b4dc2e56e72e44f90ff81b7c8dbf03348d7
Young-Jun-Park18 <dudwns4619@naver.com>
2024-12-24 16:28:42 +0900

    객체 생성, 객체 호출, 이벤트에 대하여

--- lectures/1_first-app/main.js
+++ lectures/1_first-app/main.js
@@ -14,3 +14,31 @@ const PI = 3.14; // const는 불변 상수에 사용 + 대문자로 써야함
 
 // ${}과 백틱``
 console.log(`백틱 안에는 변수를 넣을 수 있습니다. 제 이름은 ${name} 입니다`);
+
+// 함수 선언
+function sayName() {
+  console.log("안녕하세요");
+  console.log("제 이름은");
+  console.log(`${name} 입니다.`);
+}
+
+// 함수 호출
+sayName();
+
+// 객체 생성
+let obj = {
+  //객체 변수
+  age: 30, // 프로퍼티(객체가 가진 속성)
+  // 객체 함수
+  sayHello: function () {
+    console.log("hello1");
+    console.log("hello2");
+    console.log("hello3");
+  },
+};
+
+// 객체 호출
+console.log(obj.age); // 부분 출력
+obj.sayHello(); // 객체 함수 출력
+
+// 이벤트란 HTML 요소에서 발생한 사건
